395d49cc257ffabfbdf754466106b55f7c1bf69b
nckt <a_17underdogs@yahoo.co.jp>
2015-07-20 00:43:59 +0900

    テスト

---
+++ src/world.py
@@ -0,0 +1,11 @@
+#---------------------------------------------------
+# Name:        world.py
+# Purpose:     life game server process.
+#
+# Author:      nckt
+# Licence:     Public Domain
+#---------------------------------------------------
+#!/usr/bin/env python
+
+class World:
+    def __init__(self)
