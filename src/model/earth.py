3dfd87e3ecc167a13a241bf444ba7256fefd50d2
junsuke nakakita <junsuke@www6217uo.sakura.ne.jp>
2015-08-18 12:11:21 +0900

    delete debug

--- src/model/earth.py
+++ src/model/earth.py
@@ -42,31 +42,17 @@ class earth :
         @param  int y coordinate.
         @return int Count.
         """
-        # debug #
-        print("======")
-        # debug #
+
         count = 0
 
         for y in range(argY - 1, argY + 2) :
             for x in range(argX - 1, argX + 2):
-                # debug #
-                print([x,y])
-                # debug #
                 if y == argY and x == argX :
-                    # debug #
-                    print("skip!")
-                    # debug #
                     continue
 
                 if self.__isLiving(x, y) :
-                    # debug #
-                    print("living. count++")
-                    # debug #
                     count += 1
 
-                # debug #
-                print("-----")
-                # debug #
         return count
 
     def __isLiving(self, x, y) :
